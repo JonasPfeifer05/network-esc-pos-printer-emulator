c72324d09ea2f6ab350e7606dd657c92b17d191f
jonas <jonas.pfeifer@htlstp.at>
2024-10-23 12:58:40 +0200

    added port selection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::Parser;
+use log::{info, LevelFilter};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Arguments {
+    #[arg(short, long)]
+    port: Option<u64>
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::builder().filter_level(LevelFilter::Debug).init();
+
+    let arguments: Arguments = Arguments::parse();
+
+    let port;
+    match arguments.port {
+        None => {
+            info!("No port was provided");
+            info!("Therefore using default port 9100");
+            port = 9100;
+        }
+        Some(user_port) => {
+            port = user_port;
+        }
+    }
+    info!("Starting esc/pos emulator on port {port}")
 }
